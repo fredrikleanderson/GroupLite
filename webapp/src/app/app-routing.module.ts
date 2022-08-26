42b5c14a028641745c545a8ebbad51d97a9eff4c
leanderson.fredrik@gmail.com <leanderson.fredrik@gmail.com>
2022-08-26 09:14:07 +0200

    Navigation works

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './components/about/about-page/about-page.component';
+import { ContactPageComponent } from './components/contact/contact-page/contact-page.component';
+import { CreatePageComponent } from './components/create/create-page/create-page.component';
+import { LoadPageComponent } from './components/load/load-page/load-page.component';
+import { StartPageComponent } from './components/start/start-page/start-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'start', component: StartPageComponent},
+  {path: 'create', component: CreatePageComponent},
+  {path: 'load', component: LoadPageComponent},
+  {path: 'about', component: AboutPageComponent},
+  {path: 'contact', component: ContactPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AboutPageComponent } from './components/about/about-page/about-page.component';
+import { ContactPageComponent } from './components/contact/contact-page/contact-page.component';
+import { CreatePageComponent } from './components/create/create-page/create-page.component';
+import { LoadPageComponent } from './components/load/load-page/load-page.component';
+import { StartPageComponent } from './components/start/start-page/start-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    CreatePageComponent,
+    LoadPageComponent,
+    StartPageComponent
   ],
   imports: [
     BrowserModule,

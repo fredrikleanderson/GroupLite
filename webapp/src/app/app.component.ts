b33504ec66928a9af5db2bd5e32f30853fbaced7
leanderson.fredrik@gmail.com <leanderson.fredrik@gmail.com>
2022-09-28 10:27:35 +0200

    Group cards och secondary outlet

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -12,6 +12,6 @@ export class AppComponent implements OnInit {
   constructor(private router:Router) { }
 
   ngOnInit(): void {
-    this.router.navigateByUrl('create')
+    this.router.navigateByUrl('start')
   }
 }

33033ac5364ef6cd111b60ef2102ced0ba0e12d2
leanderson.fredrik@gmail.com <leanderson.fredrik@gmail.com>
2022-09-30 20:10:27 +0200

    Ready for deploy

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutPageComponent } from './components/about/about-page/about-page.component';
-import { ContactPageComponent } from './components/contact/contact-page/contact-page.component';
 import { ControlPageComponent } from './components/control/control-page/control-page.component';
 import { GroupingComponent } from './components/control/grouping/grouping.component';
 import { OverviewComponent } from './components/control/overview/overview.component';
@@ -14,7 +13,6 @@ const routes: Routes = [
   {path: 'create', component: CreatePageComponent},
   {path: 'load', component: LoadPageComponent},
   {path: 'about', component: AboutPageComponent},
-  {path: 'contact', component: ContactPageComponent},
   {path: 'control', component: ControlPageComponent, children: [
     {path: 'overview', component: OverviewComponent, outlet: 'secondary'},
     {path: 'grouping', component: GroupingComponent, outlet: 'secondary'}
